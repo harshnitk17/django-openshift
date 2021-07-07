bb959587595a84b33b5ddc1b7a16edf224a2d432
Harsh Prakash Gupta <harsh.prakash.gupta@cern.ch>
2021-07-07 13:36:23 +0530

    filter1 working

--- b2charm/forms.py
+++ b2charm/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class FilterForm(forms.Form):
-    CHOICES=[('Bd','Bd'),('Bu','Bu'),('B','B'),('Bs','Bs'),('Bc','Bc'),('Bbaryon','Bbaryon')]
-    CHOICES2=[('D','single charm mesons'),('DD','two charm mesons'),('cc','charmonium'),('baryon','charm baryon'),('other','charm like states'),('B','beauty meson')]
-    initial = forms.ChoiceField(label='Initial State',choices=CHOICES, widget=forms.RadioSelect)
-    final = forms.ChoiceField(label='Final State',choices=CHOICES2, widget=forms.RadioSelect)
+    CHOICES=[('Bd','Bd'),('Bu','Bu'),('B','B'),('Bs','Bs'),('Bc','Bc'),('Lambdab','Lambdab'),('Xib','Xib'),('Omegab','Omegab')]
+    #CHOICES2=[('D','single charm mesons'),('DD','two charm mesons'),('cc','charmonium'),('baryon','charm baryon'),('other','charm like states'),('B','beauty meson')]
+    initial = forms.ChoiceField(label='Initial Particles',choices=CHOICES, widget=forms.RadioSelect)
+    #final = forms.ChoiceField(label='Final State',choices=CHOICES2, widget=forms.RadioSelect)
 
--- b2charm/views.py
+++ b2charm/views.py
@@ -14,15 +14,18 @@ def post_form(request):
         form = FilterForm(request.POST)
         if form.is_valid():
             initial = form.cleaned_data.get('initial')
-            final = form.cleaned_data.get('final')
-            print (initial,"/",final)
-            config = {
-                "initial":initial,
-                "final":final
-            }
-            json_res = json.dumps(config)
-            return JsonResponse(json_res, content_type="application/json",status=200)
+            results = Parameters.objects.all()
+            dic={}
+            result_json = []
+            for obj in results:
+                dic[str(obj.id)] = obj.data
+            for item in dic:
+                if 'filter' in dic[item].keys():
+                    if str(initial) in dic[item]['filter']:
+                        result_json.append(dic[item])
+            return JsonResponse(json.dumps(result_json), safe = False, content_type="application/json", status=200)
         else:
             return JsonResponse(json.dumps({"some error"}),
             content_type="application/json", status=400)
+
 
