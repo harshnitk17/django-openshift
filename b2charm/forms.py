7dc2ced89e4bd4f2bbad542da4e1f6065d6871dc
Harsh Prakash Gupta <harsh.prakash.gupta@cern.ch>
2021-07-08 14:14:24 +0530

    added more forms

--- b2charm/forms.py
+++ b2charm/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 
-class FilterFormInitial(forms.Form):
-    CHOICES=[('Bd','Bd'),('Bu','Bu'),('B','B'),('Bs','Bs'),('Bc','Bc'),('Lambdab','Lambdab'),('Xib','Xib'),('Omegab','Omegab')]
-    initial = forms.ChoiceField(label='Initial Particles',choices=CHOICES, widget=forms.RadioSelect(attrs={'class': 'initialform'}))
 
-class FilterFormObservable(forms.Form):
-    CHOICES=[('BR','BR'), ('ACP','ACP'), ('polarization','polarization')]
-    observable = forms.ChoiceField(label='Type of observable',choices=CHOICES, widget=forms.RadioSelect)
-
-
-
-
+class FilterForm(forms.Form):
+    CHOICES_initial = [('Bd', 'Bd'), ('Bu', 'Bu'), ('B', 'B'), ('Bs', 'Bs'),
+                       ('Bc', 'Bc'), ('Lambdab', 'Lambdab'), ('Xib', 'Xib'), ('Omegab', 'Omegab')]
+    initial = forms.ChoiceField(required=False, label='Initial Particles',
+                                choices=CHOICES_initial, widget=forms.RadioSelect(attrs={'class': 'formbtn'}))
+    CHOICES_observable = [('BR', 'BR'), ('ACP', 'ACP'),
+                          ('polarization', 'polarization')]
+    observable = forms.ChoiceField(required=False, label='Type of observable',
+                                   choices=CHOICES_observable, widget=forms.RadioSelect(attrs={'class': 'formbtn'}))
+    other_particles = ['D0', 'D+', 'Ds', 'D*0', 'D*+', 'Ds*', 'D**', 'Ds**', 'Jpsi', 'psi2S', 'etac', 'chic', 'ccbar', 'X3872', 'Pc', 'XYZ',
+                       'p', 'n', 'Lambda', 'Sigma', 'Xi', 'Omega', 'baryon', 'Lambdac', 'Sigmac', 'Xic', 'cbaryon',
+                       'K', 'K0', 'K*+', 'K*0', 'phi', 'kaon', 'pi', 'pi0', 'rho+', 'rho0', 'eta', 'omega', 'light', 'lepton', 'photon']
+    D0 = forms.IntegerField(required=False, label="D0")
+    Dplus = forms.IntegerField(required=False, label="D+")
+    Ds = forms.IntegerField(required=False, label="Ds")
+    Ds0 = forms.IntegerField(required=False, label="D*0")
+    Dsplus = forms.IntegerField(required=False, label="D*+")
+    Dss = forms.IntegerField(required=False, label="Ds*")
+    Dsss = forms.IntegerField(required=False, label="D**")
+    Dssss = forms.IntegerField(required=False, label="Ds**")
--- b2charm/views.py
+++ b2charm/views.py
@@ -1,32 +1,44 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Parameters
-from .forms import FilterFormInitial
+from .forms import FilterForm
 import json
 
+
 def index(request):
-    form = FilterFormInitial()
-    return render(request, "index.html", {'form':form })
+    form = FilterForm()
+    return render(request, "index.html", {'form': form})
+
 
 def post_form(request):
     if request.is_ajax and request.method == "POST":
-        form = FilterFormInitial(request.POST)
+        form = FilterForm(request.POST)
         if form.is_valid():
-            initial = form.cleaned_data.get('initial')
+            try:
+                initial = form.cleaned_data.get('initial')
+            except:
+                initial = None
+            try:
+                observable = form.cleaned_data.get('observable')
+            except:
+                observable = None
             results = Parameters.objects.all()
-            dic={}
+            dic = {}
             result_json = []
             for obj in results:
                 dic[str(obj.id)] = obj.data
             for item in dic:
                 if 'filter' in dic[item].keys():
-                    if str(initial) in dic[item]['filter']:
+                    if initial and observable and str(initial) in dic[item]['filter'] and str(observable) in dic[item]['filter']:
                         result_json.append(dic[item])
+                    elif initial and not observable and str(initial) in dic[item]['filter']:
+                        result_json.append(dic[item])
+                    elif not initial and observable and str(observable) in dic[item]['filter']:
+                        result_json.append(dic[item])
+
             del results
             del dic
-            return JsonResponse(json.dumps(result_json), safe = False, content_type="application/json", status=200)
+            return JsonResponse(json.dumps(result_json), safe=False, content_type="application/json", status=200)
         else:
             return JsonResponse(json.dumps({"some error"}),
-            content_type="application/json", status=400)
-
-
+                                content_type="application/json", status=400)
