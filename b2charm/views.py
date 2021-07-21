c9121894446e0f502f6c5a6f92e121d1690b4ca5
Harsh Prakash Gupta <harsh.prakash.gupta@cern.ch>
2021-07-21 23:24:52 +0530

    fixed detail page

--- b2charm/views.py
+++ b2charm/views.py
@@ -102,6 +102,19 @@ def latex_result_index(unit, digits, value, error):
     return result
 
 
+def latex_result(unit, digits, value, stat_error, syst_error=None):
+    """Return latex code for a measurement/average result in the given unit with "digits" digits after the dot."""
+
+    digits = max(0, digits)
+    if stat_error is None:
+        return f'< {value/unit:.{digits}f}'
+    result = f'{(value/unit):.{digits}f}'
+    result += latex_error(unit, digits, stat_error)
+    if syst_error is not None:
+        result += latex_error(unit, digits, syst_error)
+    return result
+
+
 def view_detail(request, id):
     par = Parameters.objects.filter(data__id=id).first()
     image_url = str(id)+".png"
@@ -117,30 +130,94 @@ def view_detail(request, id):
             error.append(float(par.data['error']))
         else:
             error = None
-    pdg_error = []
-    if 'pdg_error_pos' in par.data.keys():
-        pdg_error.append(float(par.data['pdg_error_pos']))
-        pdg_error.append(float(par.data['pdg_error_neg']))
-    else:
-        if 'pdg_error' in par.data.keys():
-            pdg_error.append(float(par.data['pdg_error']))
+    if 'pdg_value' in par.data.keys():
+        pdg_error = []
+        if 'pdg_error_pos' in par.data.keys():
+            pdg_error.append(float(par.data['pdg_error_pos']))
+            pdg_error.append(float(par.data['pdg_error_neg']))
         else:
-            pdg_error = None
-    pdg_val = float(par.data['pdg_value'])
+            if 'pdg_error' in par.data.keys():
+                pdg_error.append(float(par.data['pdg_error']))
+            else:
+                pdg_error = None
+        pdg_val = float(par.data['pdg_value'])
+        pdg_val = "$" + str(latex_result_index(unit,
+                            digits, pdg_val, pdg_error))+"$"    
+        pdg_link = par.data['pdg_link']
+        print (pdg_val)
+    else:
+        pdg_val = None
+        pdg_link = None
     avg = "$" + str(latex_result_index(unit, digits, value, error))+"$"
-    pdg_val = "$" + str(latex_result_index(unit, digits, pdg_val, pdg_error))+"$"
-    chi2_avg = round(float(par.data['chi2']),2)
+    chi2_avg = round(float(par.data['chi2']), 2)
     p = float(par.data['p'])
     p = '%.2E' % Decimal(p)
-    if p[5]=='-':
-        power=str(int(p[5:]))
+    if p[5] == '-':
+        power = str(int(p[5:]))
     else:
         power = str(int(p[6:]))
     p = "$"+"p="+p[:4]+"\\times 10^{"+power+"}$"
     ndf_avg = int(par.data['ndf'])
-    pdg_link = par.data['pdg_link']
     measurements = par.data['measurements']
-    
-    return render(request, "detail.html", {'title': id, 'id': id, 'image_url': image_url, 'latex': par.data['latex'], 
-            'unit': unit, 'avg': avg, 'chi2_avg':chi2_avg,'ndf_avg':ndf_avg,'p':p,'pdg_value':pdg_val,'pdg_link':pdg_link,
-            'measurements':a,})
+    measurements_list = []
+    measurements_red_list = []
+    del digits
+    for measurement in measurements:
+        if 'color' in measurement.keys() and measurement['color'] == "red":
+            dic = {}
+            dic["experiment"] = measurement['experiment']
+            dic['link'] = measurement['link']
+            dic['text'] = measurement['text']
+            dic['chi2'] = round(measurement['chi2'],2)
+            if 'comments' in measurement.keys():
+                dic['comments'] = measurement['comments']
+            else:
+                dic['comments']= None
+            if 'latex' in measurement.keys():
+                dic['measurement'] = "$"+measurement['latex']+"$"
+            measurements_red_list.append(dic)
+        else:
+            dic = {}
+            dic["experiment"] = measurement['experiment']
+            dic['link'] = measurement['link']
+            dic['text'] = measurement['text']
+            dic['chi2'] = round(measurement['chi2'],2)
+            if 'comments' in measurement.keys():
+                dic['comments'] = measurement['comments']
+            else:
+                dic['comments'] = None
+            if 'latex' in measurement.keys():
+                dic['measurement'] = "$"+measurement['latex']+"$"
+            else:
+                stat_error = []
+                if 'stat_error_pos' in measurement.keys():
+                    stat_error.append(float(measurement['stat_error_pos']))
+                    stat_error.append(float(measurement['stat_error_neg']))
+                else:
+                    if 'stat_error' in measurement.keys():
+                        stat_error.append(float(measurement['stat_error']))
+                    else:
+                        stat_error = None
+                syst_error = []
+                if 'syst_error_pos' in measurement.keys():
+                    syst_error.append(float(measurement['syst_error_pos']))
+                    syst_error.append(float(measurement['syst_error_neg']))
+                else:
+                    if 'syst_error' in measurement.keys():
+                        syst_error.append(float(measurement['syst_error']))
+                    else:
+                        syst_error = None
+                mes_val = float(measurement['value'])
+                digits = int(measurement['digits'])
+                if 'using' in measurement.keys():
+                    dic['measurement'] = "$" + str(latex_result(
+                        unit, digits, mes_val, stat_error, syst_error))+"$"+" using "+measurement['using']
+                else:
+                    dic['measurement'] = "$" + \
+                        str(latex_result(unit, digits, mes_val,
+                            stat_error, syst_error))+"$"
+            measurements_list.append(dic)
+
+    return render(request, "detail.html", {'title': id, 'id': id, 'image_url': image_url, 'latex': par.data['latex'],
+                                           'unit': unit, 'avg': avg, 'chi2_avg': chi2_avg, 'ndf_avg': ndf_avg, 'p': p, 'pdg_value': pdg_val, 'pdg_link': pdg_link,
+                                           'measurement_list': measurements_list, 'measurement_red_list': measurements_red_list, })
