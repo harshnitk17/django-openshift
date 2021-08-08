23f5dc668fff3eeabe5ab5e3fb42ebda67dfe782
Harsh Prakash Gupta <harsh.prakash.gupta@cern.ch>
2021-08-08 19:20:35 +0530

    updated test

--- b2charm/tests.py
+++ b2charm/tests.py
@@ -1,8 +1,1 @@
-from django.test import TestCase
-import os
-
-# These basic tests are to be used as an example for running tests in S2I
-# and OpenShift when building an application image.
-class PageViewModelTest(TestCase):
-    def test_viewpage_model(self):
-        self.assertEqual(1,1)
+print ("hello world")
